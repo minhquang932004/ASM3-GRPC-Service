ba651e8027adeccb60c3bc2497beeab6a107b8b5
minhquang932004 <minhquang932004@gmail.com>
2025-10-19 21:56:35 +0700

    Add project files.

---
+++ ElectricVehicleM.GrpcService.QuangNM/Program.cs
@@ -0,0 +1,17 @@
+using ElectricVehicleM.GrpcService.QuangNM.Services;
+using ElectricVehicleM.Services.QuangNM;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddScoped<IServiceProviders, ServiceProviders>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGrpcService<PromotionsQuangNmGRPCService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ ElectricVehicleM.GrpcService.QuangNM/Services/PromotionsQuangNmGRPCService.cs
@@ -0,0 +1,43 @@
+﻿using ElectricVehicleM.GrpcService.QuangNM.Protos;
+using ElectricVehicleM.Services.QuangNM;
+using Grpc.Core;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ElectricVehicleM.GrpcService.QuangNM.Services
+{
+    public class PromotionsQuangNmGRPCService : PromotionsQuangNmGRPC.PromotionsQuangNmGRPCBase
+    {
+        private readonly IServiceProviders _serviceProviders;
+        public PromotionsQuangNmGRPCService(IServiceProviders serviceProviders)
+        {
+            _serviceProviders = serviceProviders;
+        }
+
+        public override async Task<PromotionsQuangNmList> GetAllAsync(EmptyRequest request, ServerCallContext context)
+        {
+            // Initialize result
+            var result = new PromotionsQuangNmList();
+
+            try
+            {
+                // Get data from service
+                var promotions = await _serviceProviders.PromotionsQuangNmService.GetAllAsync();
+                // Serialize with options to handle reference loops and ignore nulls
+                var opt = new JsonSerializerOptions() { ReferenceHandler = ReferenceHandler.IgnoreCycles, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+                // Convert to JSON string
+                var promotionsJson = JsonSerializer.Serialize(promotions, opt);
+                // Deserialize JSON string to list of PromotionsQuangNm
+                var items = JsonSerializer.Deserialize<List<PromotionsQuangNm>>(promotionsJson, opt);
+                // Add items to result
+                result.Items.AddRange(items);
+            }
+            catch (Exception ex)
+            {
+                throw new RpcException(new Status(StatusCode.Internal, ex.Message));
+            }
+
+            return result;
+        }
+    }
+}
